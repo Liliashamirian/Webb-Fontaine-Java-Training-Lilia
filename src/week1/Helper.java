2d523ee4e491eea173c5d26d7fb8713b632e4844
Lilia Shamirian <lilia.shamirian@webbfontaine.com>
2020-09-14 15:04:38 +0400

    Added Week One Homework.

--- src/week1/Helper.java
+++ src/week1/Helper.java
@@ -6,9 +6,10 @@ import java.util.Scanner;
  * Created by Lilia Shamirian on  13, Sep, 2020
  */
 public class Helper {
-    public static boolean isDataValid(Scanner scanner){
+    public static boolean isDataValid(Scanner scanner) {
         if (!(scanner.hasNextInt()) || !(scanner.hasNextFloat()) || !(scanner.hasNextDouble())) {
             System.out.println("Please Enter A Valid Input");
+            return false;
         }
         return true;
     }
